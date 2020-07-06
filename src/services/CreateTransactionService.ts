f03d8a84bf8f7a341220fafdd1b57976439827b0
Camila Sales <salescamila1@gmail.com>
2020-07-06 17:41:29 -0400

    feat: Check total available in acount

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -7,6 +7,13 @@ interface RequestDTO {
   type: 'income' | 'outcome';
 }
 
+interface Balance {
+  // id: string;
+   income: number;
+   outcome: number;
+   total: number;
+ }
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -15,6 +22,15 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: RequestDTO): Transaction {
+    // Check total available in acount
+    if (type == 'outcome'){
+      const balance = this.transactionsRepository.getBalance();
+      if ( value > balance.total ){
+        throw new Error('Insufficient funds!');
+      }
+    }
+
+    // Create transaction
     const transaction = this.transactionsRepository.create({
       title,
       value,
